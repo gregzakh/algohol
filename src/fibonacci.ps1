64857459e4acf75fe714b7e65a504c9f9f6963c0
gregzakh <grishanz@yandex.ru>
2020-11-30 23:16:40 +0500

    A  src/fibonacci.ps1

---
+++ src/fibonacci.ps1
@@ -0,0 +1,19 @@
+using namespace System.Windows.Media
+
+Add-Type -AssemblyName WindowsBase
+
+function fib_matrix([UInt16]$n) {
+  end {
+    $m1, $m2 = [Matrix]::new(1,0,0,1,0,0), [Matrix]::new(1,1,1,0,0,0)
+    for ($i = 1; $i -lt $n; $i++) { $m1 *= $m2 }
+    $m1.M11
+  }
+}
+
+function fib_iter([UInt16]$n) {
+  end {
+    $res = 0, 1
+    while ($res.Length -le $n) { $res += $res[-1] + $res[-2] }
+    $res[-1]
+  }
+}
