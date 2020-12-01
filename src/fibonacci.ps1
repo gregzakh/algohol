fb9d7bd82c21cd31ea4cf4294b08302f20747334
gregzakh <grishanz@yandex.ru>
2020-12-01 21:47:57 +0500

    M src/fibonacci.ps1

--- src/fibonacci.ps1
+++ src/fibonacci.ps1
@@ -13,7 +13,10 @@ function fib_matrix([UInt16]$n) {
 function fib_iter([UInt16]$n) {
   end {
     $res = 0, 1
-    while ($res.Length -le $n) { $res += $res[-1] + $res[-2] }
+    while (--$n) {
+      $res += $res[-1] + $res[-2]
+      $res = $res[1, 2]
+    }
     $res[-1]
   }
 }
