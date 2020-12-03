9248c67d051c7385072c56372996f30e671230ac
gregzakh <grishanz@yandex.ru>
2020-12-03 21:46:23 +0500

    A  src/fisher_yates_shuffle.ps1

---
+++ src/fisher_yates_shuffle.ps1
@@ -0,0 +1,11 @@
+# Fisher-Yates shuffle
+function sattolo([Object[]]$a) {
+  end {
+    for ($i = 0; $i -lt $a.Length; $i++) {
+      $j = Get-Random -Minimum 0 -Maximum $a.Length
+      $k = Get-Random -Minimum 0 -Maximum $a.Length
+      $a[$j], $a[$k] = $a[$k], $a[$j]
+    }
+    $a
+  }
+}
