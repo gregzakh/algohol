8c5f5f0818b6ed6681660595f8fad9a4f33aebce
gregzakh <grishanz@yandex.ru>
2020-12-04 23:17:10 +0500

    A  src/adler32.ps1

---
+++ src/adler32.ps1
@@ -0,0 +1,10 @@
+function adler32([String]$s) {
+  end {
+    $a, $b = [UInt32[]](1, 0)
+    for ($i = 0; $i -lt $s.Length; $i++) {
+      $a = ($a + $s[$i]) % 65521
+      $b = ($b + $a) % 65521
+    }
+    ($b -shl 16) -bor $a
+  }
+}
