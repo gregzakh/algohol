7fe8a082dc823ff711e45621e16e75b25a050ab6
gregzakh <grishanz@yandex.ru>
2020-12-06 00:00:53 +0500

    M src/gcd.ps1

--- src/gcd.ps1
+++ src/gcd.ps1
@@ -1,2 +1,5 @@
 # greatest common divisor
-function gcd($x, $y) {[Math]::Abs((.({gcd $y ($x % $y)}, {$x})[!$y]))}
+# recursive
+function gcd_r($x, $y) {[Math]::Abs((.({gcd_r $y ($x % $y)}, {$x})[!$y]))}
+# iterative
+function gcd_i($x, $y) {while($y){$x, $y = $y, ($x % $y)};[Math]::Abs($x)}
