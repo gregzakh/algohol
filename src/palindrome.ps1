60996acba9e0af72253213ef98c9c651756a9eaf
gregzakh <grishanz@yandex.ru>
2020-12-06 21:49:35 +0500

    A  src/palindrome.ps1

---
+++ src/palindrome.ps1
@@ -0,0 +1,12 @@
+#requires -version 7
+# palindrome test
+# .e.g.: Madam, I'm Adam!
+#        Sum summus mus
+# and etc.
+function palindrome([String]$x) {
+  end {
+    (-join[Linq.Enumerable]::Reverse(
+      ($$ = $x -replace '[^\p{L}]').ToLower()
+    )) -eq $$
+  }
+}
